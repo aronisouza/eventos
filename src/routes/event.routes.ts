9790c8f62a3f4a3aeca235b5f11adf3fd3b5d888
aronisouza <aronisouza@yahoo.com.br>
2023-08-10 12:04:48 -0300

    Atualizado rota para criar evento

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,14 +1,24 @@
 import { Router } from "express";
+import { EventRepoMongoose } from "../repositories/EventRepoMongoose";
+import { EventUseCase } from "../useCases/EventUseCase";
+import { EventController } from "../controllers/EventController";
 
 class EventRoutes {
   public router: Router;
-  constructor(){
+  private eventController: EventController;
+
+  constructor() {
     this.router = Router();
+
+    const eventRepository = new EventRepoMongoose();
+    const eventUseCase = new EventUseCase(eventRepository);
+    this.eventController = new EventController(eventUseCase);
+
     this.initRoutes();
   }
 
-  initRoutes(){
-    this.router.post('/');
+  initRoutes() {
+    this.router.post('/', this.eventController.create.bind(this.eventController));
   }
 }
 export { EventRoutes };
